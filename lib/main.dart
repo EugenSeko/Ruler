1ae54cc10f04380234ee22e9e4ace369365f63ff
EugenSeko <EugenSeko@users.noreply.github.com>
2023-04-18 21:37:40 +0300

    camera init

--- lib/main.dart
+++ lib/main.dart
@@ -1,261 +1,63 @@
-import 'dart:async';
-import 'dart:math';
-
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sensors/flutter_sensors.dart';
 
-void main() => runApp(const MyApp());
+late List<CameraDescription> _cameras;
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  @override
-  _MyAppState createState() => _MyAppState();
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  bool _accelAvailable = false;
-  bool _gyroAvailable = false;
-  List<double> _accelData = List.filled(3, 0.0);
-  List<double> _gyroData = List.filled(3, 0.0);
-  StreamSubscription? _accelSubscription;
-  StreamSubscription? _gyroSubscription;
-  var stopwatch = Stopwatch();
+/// CameraApp is the Main Application.
+class CameraApp extends StatefulWidget {
+  /// Default Constructor
+  const CameraApp({super.key});
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
 
   @override
   void initState() {
-    _checkAccelerometerStatus();
-    _checkGyroscopeStatus();
     super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
   }
 
   @override
   void dispose() {
-    _stopAccelerometer();
-    _stopGyroscope();
+    controller.dispose();
     super.dispose();
-  }
-
-  void _checkAccelerometerStatus() async {
-    await SensorManager()
-        .isSensorAvailable(Sensors.ACCELEROMETER)
-        .then((result) {
-      setState(() {
-        _accelAvailable = result;
-      });
-    });
-  }
-
-  List accelArray = [];
-  double sumAccel = 0;
-  double maxAccel = 0;
-  double distance = 0;
-  double time = 0;
-  int eventCounter = 0;
-  double get avgAccel {
-    if (accelArray.isNotEmpty) {
-      return sumAccel / accelArray.length;
-    } else {
-      return 0;
-    }
-  }
-
-  Future<void> _startAccelerometer() async {
-    if (_accelSubscription != null) return;
-
-    // stopwatch.start();
-
-    Timer.periodic(const Duration(milliseconds: 1000), (timer) {
-      print('Distance ---->  ${distance * 100}');
-      // if (avgAccel > 0 || maxAccel > 0) {
-      //   time = stopwatch.elapsedMicroseconds / 1000000;
-      //   var temp = (1 / 2) * avgAccel * pow(time, 2);
-      //   distance += temp;
-      //   print('Distance ---->  $distance');
-      // }
-
-      // accelArray = [];
-      // maxAccel = 0;
-      // sumAccel = 0;
-      // time = 0;
-      // distance = 0;
-      // stopwatch.reset();
-      // stopwatch.start();
-    });
-
-    if (_accelAvailable) {
-      final stream = await SensorManager().sensorUpdates(
-        sensorId: Sensors.LINEAR_ACCELERATION,
-        interval: Sensors.SENSOR_DELAY_FASTEST,
-      );
-      _accelSubscription = stream.listen((sensorEvent) {
-        setState(() {
-          if (eventCounter == 0) {
-            stopwatch.start();
-          }
-          time = stopwatch.elapsedMicroseconds / 1000000;
-          var temp = (1 / 2) * sensorEvent.data[2] * pow(time, 2);
-          distance += temp;
-          stopwatch.reset();
-          time = 0;
-          stopwatch.start();
-          // _accelData = sensorEvent.data;
-          // if (sensorEvent.data[2].abs() >= 0) {
-          //   if (maxAccel < sensorEvent.data[2]) {
-          //     maxAccel = sensorEvent.data[2];
-          //   }
-          //   accelArray.add(sensorEvent.data[2]);
-          //   sumAccel += sensorEvent.data[2];
-          // }
-          // for (var i = 0; i < 3; i++) {
-          //   if (_accelData[i].abs() <= sensorEvent.data[i].abs()) {
-          //     _accelData[i] = sensorEvent.data[i];
-          //   }
-          // }
-        });
-      });
-    }
-  }
-
-  void _stopAccelerometer() {
-    time = stopwatch.elapsedMilliseconds.toDouble();
-
-    _accelData = List.filled(3, 0.0);
-    if (_accelSubscription == null) return;
-    _accelSubscription?.cancel();
-    _accelSubscription = null;
-  }
-
-  void _checkGyroscopeStatus() async {
-    await SensorManager().isSensorAvailable(Sensors.GYROSCOPE).then((result) {
-      setState(() {
-        _gyroAvailable = result;
-      });
-    });
-  }
-
-  Future<void> _startGyroscope() async {
-    if (_gyroSubscription != null) return;
-    if (_gyroAvailable) {
-      final stream =
-          await SensorManager().sensorUpdates(sensorId: Sensors.GYROSCOPE);
-      _gyroSubscription = stream.listen((sensorEvent) {
-        setState(() {
-          _gyroData = sensorEvent.data;
-        });
-      });
-    }
-  }
-
-  void _stopGyroscope() {
-    if (_gyroSubscription == null) return;
-    _gyroSubscription?.cancel();
-    _gyroSubscription = null;
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Sensors Example'),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(16.0),
-          alignment: AlignmentDirectional.topCenter,
-          child: Column(
-            children: <Widget>[
-              Text(
-                "Accelerometer Test",
-                textAlign: TextAlign.center,
-              ),
-              Text(
-                "Accelerometer Enabled: $_accelAvailable",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "[0](X) = ${_accelData[0]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "[1](Y) = ${_accelData[1]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "[2](Z) = ${_accelData[2]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  MaterialButton(
-                    child: Text("Start"),
-                    color: Colors.green,
-                    onPressed:
-                        _accelAvailable ? () => _startAccelerometer() : null,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                  ),
-                  MaterialButton(
-                    child: Text("Stop"),
-                    color: Colors.red,
-                    onPressed:
-                        _accelAvailable ? () => _stopAccelerometer() : null,
-                  ),
-                ],
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "Gyroscope Test",
-                textAlign: TextAlign.center,
-              ),
-              Text(
-                "Gyroscope Enabled: $_gyroAvailable",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "[0](X) = ${_gyroData[0]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "[1](Y) = ${_gyroData[1]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Text(
-                "[2](Z) = ${_gyroData[2]}",
-                textAlign: TextAlign.center,
-              ),
-              Padding(padding: EdgeInsets.only(top: 16.0)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  MaterialButton(
-                    child: Text("Start"),
-                    color: Colors.green,
-                    onPressed: _gyroAvailable ? () => _startGyroscope() : null,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                  ),
-                  MaterialButton(
-                    child: Text("Stop"),
-                    color: Colors.red,
-                    onPressed: _gyroAvailable ? () => _stopGyroscope() : null,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: CameraPreview(controller),
     );
   }
 }
