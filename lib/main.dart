85d5b6c9c7739f947011d42c013b9e4677d38ba6
EugenSeko <EugenSeko@users.noreply.github.com>
2023-04-19 14:23:22 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 late List<CameraDescription> _cameras;
 
@@ -25,6 +25,7 @@ class _CameraAppState extends State<CameraApp> {
   @override
   void initState() {
     super.initState();
+
     controller = CameraController(_cameras[0], ResolutionPreset.max);
     controller.initialize().then((_) {
       if (!mounted) {
@@ -57,7 +58,69 @@ class _CameraAppState extends State<CameraApp> {
       return Container();
     }
     return MaterialApp(
-      home: CameraPreview(controller),
+      home: Scaffold(
+          body: CameraMask(
+        controller: controller,
+      )),
+    );
+  }
+}
+
+class CameraMask extends StatefulWidget {
+  final CameraController controller;
+  CameraMask({super.key, required this.controller});
+
+  int count = 0;
+
+  @override
+  State<CameraMask> createState() => _CameraMaskState();
+}
+
+class _CameraMaskState extends State<CameraMask> {
+  @override
+  Widget build(BuildContext context) {
+    var sizes = MediaQuery.of(context).size;
+
+    var height = sizes.height;
+    var width = sizes.width;
+
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        CameraPreview(widget.controller),
+        const Center(
+          child: Icon(
+            Icons.add_circle,
+            color: Colors.white,
+          ),
+        ),
+        Positioned(
+            top: height / 3,
+            left: width / 3,
+            right: 0,
+            bottom: 0,
+            child: Container(
+                color: Colors.transparent,
+                child: TextField(
+                  decoration: const InputDecoration(hintText: 'You text here'),
+                  onChanged: (value) => print(value),
+                  onSubmitted: (value) {
+                    widget.count++;
+                  },
+                ))),
+        Positioned(
+            top: height / 4,
+            left: width / 4,
+            right: 0,
+            bottom: 0,
+            child: Container(
+                color: Colors.transparent,
+                child: Text(
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    '${widget.count}'))),
+      ],
     );
   }
 }
