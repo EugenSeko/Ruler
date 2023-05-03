e4a2e4328703d1bf64c421af03a918fae5405795
EugenSeko <EugenSeko@users.noreply.github.com>
2023-05-04 00:52:44 +0300

    observation linear acceleration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sensors/flutter_sensors.dart' as fs;
+import 'package:sensor_app/utils/sensor_manager.dart';
+import 'package:sensor_app/utils/timer.dart';
 
 void main(List<String> args) {
   runApp(MyWidget());
@@ -11,24 +14,195 @@ class MyWidget extends StatefulWidget {
   State<MyWidget> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _MyWidgetState extends State<MyWidget> {
+  SensorManager sm = SensorManager();
+  MyTimer? timer;
+  List<double> zAccels = [];
+  Stopwatch stopwatch = Stopwatch();
+  bool isReadData = false;
+  int t = 0;
+  List<double> negAccels = [];
+  List<double> posAccels = [];
+  double negSum = 0;
+  double posSum = 0;
+
+  void pullData() {
+    stopwatch.start();
+    isReadData = true;
+  }
+
+  void stopPullData() {
+    stopwatch.stop();
+    isReadData = false;
+    t = stopwatch.elapsedMilliseconds;
+    printResults();
+    stopwatch.reset();
+    zAccels.clear();
+    negAccels.clear();
+    posAccels.clear();
+    negSum = posSum = 0;
+  }
+
+  void printResults() {
+    for (var element in zAccels) {
+      element.isNegative ? negAccels.add(element) : posAccels.add(element);
+    }
+    for (var element in negAccels) {
+      // print('$element \n');
+      negSum += element;
+    }
+    for (var element in posAccels) {
+      // print('$element \n');
+      posSum += element;
+    }
+    for (var element in zAccels) {
+      print('$element \n');
+    }
+    print('Neg sum = $negSum \n');
+    print('Pos sum = $posSum \n');
+    print('Time = $t millisecounds \n');
+    print('diff = ${posSum + negSum} \n');
+    print('pos Average = ${posSum / posAccels.length} \n');
+    print('neg Average = ${negSum / negAccels.length} \n');
+    print('pos count = ${posAccels.length} \n');
+    print('neg count = ${negAccels.length} \n');
+  }
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this);
+    timer = MyTimer(() {
+      setState(() {});
+    });
+    timer?.startTimer(const Duration(milliseconds: 8));
+    sm.linearAccelDelay = fs.Sensors.SENSOR_DELAY_FASTEST;
+    sm.checkAccelerometerStatus();
+    sm.checkLinearAccelerometerStatus();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    sm.stopAll();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (isReadData) {
+      zAccels.add(sm.linearAccelData[2]);
+    }
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Sensors Example'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          alignment: AlignmentDirectional.topCenter,
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Accelerometer Test",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Accelerometer Enabled: ${sm.accelAvailable}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[0](X) = ${sm.accelData[0]}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[1](Y) = ${sm.accelData[1]}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[2](Z) = ${sm.accelData[2]}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MaterialButton(
+                    child: Text("Start"),
+                    color: Colors.green,
+                    onPressed: sm.accelAvailable
+                        ? () => sm.startAccelerometer()
+                        : null,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  MaterialButton(
+                    child: Text("Stop"),
+                    color: Colors.red,
+                    onPressed:
+                        sm.accelAvailable ? () => sm.stopAccelerometer() : null,
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "LinearAccel Test",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "LinearAccel Enabled: ${sm.linearAccelAvailable}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[0](X) = ${sm.linearAccelData[0]}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[1](Y) = ${sm.linearAccelData[1]}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Text(
+                "[2](Z) = ${sm.linearAccelData[2]}",
+                textAlign: TextAlign.center,
+              ),
+              Padding(padding: EdgeInsets.only(top: 16.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MaterialButton(
+                    child: Text("Start"),
+                    color: Colors.green,
+                    onPressed: sm.linearAccelAvailable
+                        ? () {
+                            sm.startLinearAccelerometer();
+                            pullData();
+                          }
+                        : null,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  MaterialButton(
+                    child: Text("Stop"),
+                    color: Colors.red,
+                    onPressed: sm.linearAccelAvailable
+                        ? () {
+                            sm.stopLinearAccelerometer();
+                            stopPullData();
+                          }
+                        : null,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/utils/sensor_manager.dart
+++ lib/utils/sensor_manager.dart
@@ -5,10 +5,29 @@ import 'package:flutter_sensors/flutter_sensors.dart' as fs;
 
 class SensorManager {
   List<double> _accelData = List.filled(3, 0.0);
+  List<double> _gyroData = List.filled(3, 0.0);
+  List<double> _linearAccelData = List.filled(3, 0.0);
+
   StreamSubscription? accelSubscription;
-  bool _accelAvailable = false;
+  StreamSubscription? gyroSubscription;
+  StreamSubscription? linearAccelSubscription;
+
+  bool accelAvailable = false;
+  bool gyroAvailable = false;
+  bool linearAccelAvailable = false;
+
+  Duration accelDelay = fs.Sensors.SENSOR_DELAY_GAME;
+  Duration gyroDelay = fs.Sensors.SENSOR_DELAY_GAME;
+  Duration linearAccelDelay = fs.Sensors.SENSOR_DELAY_GAME;
+
+  // bool get accelAvailable => _accelAvailable;
+  // bool get gyroAvailable => _gyroAvailable;
+  // bool get linearAccelAvailable => _linearAccelAvailable;
 
   List<double> get accelData => _accelData;
+  List<double> get gyroData => _gyroData;
+  List<double> get linearAccelData => _linearAccelData;
+
   double get tiltAngle => atan2(_accelData[1],
       sqrt(_accelData[0] * _accelData[0] + _accelData[2] * _accelData[2]));
   double get pitchAngle => atan2(-_accelData[0],
@@ -18,7 +37,23 @@ class SensorManager {
     await fs.SensorManager()
         .isSensorAvailable(fs.Sensors.ACCELEROMETER)
         .then((result) {
-      _accelAvailable = result;
+      accelAvailable = result;
+    });
+  }
+
+  void checkGyroscopeStatus() async {
+    await fs.SensorManager()
+        .isSensorAvailable(fs.Sensors.GYROSCOPE)
+        .then((result) {
+      gyroAvailable = result;
+    });
+  }
+
+  void checkLinearAccelerometerStatus() async {
+    await fs.SensorManager()
+        .isSensorAvailable(fs.Sensors.LINEAR_ACCELERATION)
+        .then((result) {
+      linearAccelAvailable = result;
     });
   }
 
@@ -28,15 +63,57 @@ class SensorManager {
     accelSubscription = null;
   }
 
+  void stopGyroscope() {
+    if (gyroSubscription == null) return;
+    gyroSubscription?.cancel();
+    gyroSubscription = null;
+  }
+
+  void stopLinearAccelerometer() {
+    if (linearAccelSubscription == null) return;
+    linearAccelSubscription?.cancel();
+    linearAccelSubscription = null;
+  }
+
+  void stopAll() {
+    stopAccelerometer();
+    stopGyroscope();
+    stopLinearAccelerometer();
+  }
+
   Future<void> startAccelerometer() async {
     if (accelSubscription != null) return;
-    if (_accelAvailable) {
+    if (accelAvailable) {
       final stream = await fs.SensorManager().sensorUpdates(
         sensorId: fs.Sensors.ACCELEROMETER,
-        interval: fs.Sensors.SENSOR_DELAY_FASTEST,
+        interval: accelDelay,
       );
       accelSubscription = stream.listen((sensorEvent) {
         _accelData = sensorEvent.data;
+      });
+    }
+  }
+
+  Future<void> startGyroscope() async {
+    if (gyroSubscription != null) return;
+    if (gyroAvailable) {
+      final stream = await fs.SensorManager()
+          .sensorUpdates(sensorId: fs.Sensors.GYROSCOPE, interval: gyroDelay);
+      gyroSubscription = stream.listen((sensorEvent) {
+        _gyroData = sensorEvent.data;
+      });
+    }
+  }
+
+  Future<void> startLinearAccelerometer() async {
+    if (linearAccelSubscription != null) return;
+    if (linearAccelAvailable) {
+      final stream = await fs.SensorManager().sensorUpdates(
+        sensorId: fs.Sensors.LINEAR_ACCELERATION,
+        interval: linearAccelDelay,
+      );
+      linearAccelSubscription = stream.listen((sensorEvent) {
+        _linearAccelData = sensorEvent.data;
       });
     }
   }
